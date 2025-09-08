2939fa14422fdb137dbbdf6106f9368b0ea704a0
Matt Olson <olsonm2027@student.faytechcc.edu>
2025-09-08 00:59:21 +0000

    still tired but program should say "Hello" now

--- m1t1.cpp
+++ m1t1.cpp
@@ -6,3 +6,18 @@ olsonm
 Program will greet the user.
 */
 
+//magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    //input: start the program
+    //processing: none
+    //output : 0  (no errors)
+    //special FX: says "Hello" to the user
+
+    // let's say "Hello"
+    cout << "Hello, user!" << endl; //"end line" not "end 1"
+
+    return 0; //no errors
+}
